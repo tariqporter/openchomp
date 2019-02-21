d7044cea300886e9ad74ce875d103b8218402436
Tariq Porter <tic084@gmail.com>
2019-02-20 23:40:30 -0500

    Clean resources

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React, { PureComponent } from 'react';
 import ContentCreator from './components/ContentCreator';
 import { withStyles } from '@material-ui/core';
-import '../node_modules/react-grid-layout/css/styles.css';
-import '../node_modules/react-resizable/css/styles.css';
 
 const styles = () => ({
   root: {
